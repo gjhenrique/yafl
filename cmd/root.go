7abf40012884aed08fd52f383c511304b2b096e1
Guilherme Henrique <gjhenrique@gmail.com>
2022-12-04 22:00:18 +0100

    Remove Long support

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "lfzf",
-	Short: "Launcher using fzf and modes",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Launcher using fzf with modes",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) { fmt.Println("hihi") },
